430f7fff4b16612269c8f221212d0b34692070a8
deathcap <deathcap@gmx.co.uk>
2016-04-09 12:51:14 -0700

    Convert to native ES6, remove CoffeeScript

--- craftingrecipes.js
+++ craftingrecipes.js
@@ -1,195 +1,291 @@
+'use strict';
 
-class CraftingThesaurus
-  constructor: () ->
-    @map = {}
-    global.CraftingThesaurus_instance = this # TODO: desingletonify?
+class CraftingThesaurus {
+  constructor() {
+    this.map = {};
+    global.CraftingThesaurus_instance = this; // TODO: desingletonify?
+  }
 
-  registerName: (lookupName, item) ->
-    throw "craftingrecipes registerName(#{lookupName}): name is in invalid format, should be 'shape.material'" if lookupName.indexOf('.') == -1
+  registerName(lookupName, item) {
+    if (lookupName.indexOf('.') === -1) {
+      throw new Error(`craftingrecipes registerName(${lookupName}): name is in invalid format, should be 'shape.material'`);
+    }
 
-    @map[lookupName] = [] if not @map[lookupName]?
-    @map[lookupName].push(item)
+    if (this.map[lookupName] === undefined) {
+      this.map[lookupName] = [];
+    }
 
-  matchesName: (lookupName, itemPile) ->
-    return true if lookupName == undefined && itemPile == undefined # nothing matches nothing
-    return false if not itemPile?
-    return true if itemPile.item == lookupName  # direct name match
+    this.map[lookupName].push(item);
+  }
+
+  matchesName(lookupName, itemPile) {
+    if (lookupName === undefined && itemPile === undefined) return true; // nothing matches nothing
+    if (itemPile === undefined) return false;
+    if (itemPile.item === lookupName) return true; // direct name match
    
-    # known alias?
-    a = @map[lookupName]
-    return false if not a?
-    return a.indexOf(itemPile.item) != -1
+    // known alias?
+    const a = this.map[lookupName];
+    if (a === undefined) return false;
+    return a.indexOf(itemPile.item) != -1;
+  }
+}
 
-class Recipe
-  computeOutput: (inventory) -> undefined
-  matches: (inventory) -> @computeOutput(inventory) != undefined
-  craft: (inventory) -> undefined
+class Recipe {
+  computeOutput(inventory) {
+    return undefined;
+  }
 
-class AmorphousRecipe extends Recipe
-  constructor: (@ingredients, @output) ->
+  matches(inventory) {
+    return this.computeOutput(inventory) !== undefined;
+  }
 
-  # if itemPile is in pendingIngredients, remove it
-  removeIngredient: (itemPile, pendingIngredients) ->
-    for testIngredient, i in pendingIngredients
-      if global.CraftingThesaurus_instance.matchesName(testIngredient, itemPile)
-        pendingIngredients.splice(i, 1)
-        return true
-    return false
+  craft(inventory) {
+    return undefined;
+  }
+}
 
+class AmorphousRecipe extends Recipe {
+  constructor(ingredients, output) {
+    super();
+    this.ingredients = ingredients;
+    this.output = output;
+  }
 
-  findMatchingSlots: (inventory) ->
-    pendingIngredients = @ingredients[..]
-    foundIndices = []
+  // if itemPile is in pendingIngredients, remove it
+  removeIngredient(itemPile, pendingIngredients) {
+    for (let i = 0; i < pendingIngredients.length; ++i) {
+      const testIngredient = pendingIngredients[i];
 
-    for i in [0...inventory.size()]
-      itemPile = inventory.get(i)
-      continue if not itemPile?
+      if (global.CraftingThesaurus_instance.matchesName(testIngredient, itemPile)) {
+        pendingIngredients.splice(i, 1);
+        return true;
+      }
+    }
 
-      if not @removeIngredient(itemPile, pendingIngredients)
-        # found something we didn't want
-        return undefined
+    return false;
+  }
 
-      foundIndices.push(i)
+  findMatchingSlots(inventory) {
+    const pendingIngredients = this.ingredients.slice();
+    const foundIndices = [];
 
-    if pendingIngredients.length != 0
-      # didn't find everything
-      return undefined
+    for (let i = 0; i < inventory.size(); ++i)  {
+      const itemPile = inventory.get(i)
+      if (itemPile === undefined) continue;
 
-    return foundIndices
+      if (!this.removeIngredient(itemPile, pendingIngredients)) {
+        // found something we didn't want
+        return undefined;
+      }
+
+      foundIndices.push(i);
+    }
+
+    if (pendingIngredients.length !== 0) {
+      // didn't find everything
+      return undefined;
+    }
+
+    return foundIndices;
+  }
    
-  computeOutput: (inventory) ->
-    return @output.clone() if @findMatchingSlots(inventory) != undefined
-    undefined
+  computeOutput(inventory) {
+    if (this.findMatchingSlots(inventory) !== undefined) {
+      return this.output.clone();
+    }
+    return undefined;
+  }
 
-  craft: (inventory) ->
-    slots = @findMatchingSlots(inventory)
-    return undefined if !slots
+  craft(inventory) {
+    const slots = this.findMatchingSlots(inventory);
+    if (!slots) return undefined;
 
-    for slot in slots
-      inventory.takeAt slot, 1 # TODO: check return
+    for (let slot of slots) {
+      inventory.takeAt(slot, 1); // TODO: check return
+    }
 
-    return @output.clone()
+    return this.output.clone();
+  }
+}
 
-class PositionalRecipe extends Recipe
-  # TODO: accept patternStrArray, ingredientMap (['AB''], {A:.., B:..})
-  constructor: (@ingredientMatrix, @output) ->
+class PositionalRecipe extends Recipe {
+  // TODO: accept patternStrArray, ingredientMap (['AB''], {A:.., B:..})
+  constructor(ingredientMatrix, output) {
+    super();
+    this.ingredientMatrix = ingredientMatrix;
+    this.output = output;
+  }
 
-  findMatchingSlots: (inputInventory) ->
-    # inventory input ingredients must match @ingredientMatrix at same (relative) positions
-    foundIndices = []
+  findMatchingSlots(inputInventory) {
+    // inventory input ingredients must match this.ingredientMatrix at same (relative) positions
+    const foundIndices = [];
 
-    [inventory, shiftRow, shiftColumn] = PositionalRecipe.tighten inputInventory
-    if inventory.height != @ingredientMatrix.length or inventory.width != @ingredientMatrix[0].length
-      return undefined
+    const a = PositionalRecipe.tighten(inputInventory);
+    const inventory = a[0];
+    const shiftRow = a[1];
+    const shiftColumn = a[2];
 
-    for row, i in @ingredientMatrix
-      for expectedName, j in row
-        index = j + i * inventory.width
-        actualPile = inventory.get(index)
+    if (inventory.height !== this.ingredientMatrix.length || inventory.width !== this.ingredientMatrix[0].length) {
+      return undefined;
+    }
 
-        if not global.CraftingThesaurus_instance.matchesName(expectedName, actualPile)
-          return undefined
+    for (let i = 0; i < this.ingredientMatrix.length; ++i) {
+      const row = this.ingredientMatrix[i];
 
-        # add original index from inputInventory for removing ingredients
-        unshiftedIndex = (j + shiftColumn) + (i + shiftRow) * inputInventory.width
-        foundIndices.push(unshiftedIndex)
+      for (let j = 0; j < row.length; ++j) {
+        const expectedName = row[j];
 
-    return foundIndices
+        const index = j + i * inventory.width;
+        const actualPile = inventory.get(index);
 
-  # tighten the bounds around an inventory, shrinking it from the edges if possible
-  @tighten = (inventory) ->
-    # iterate from each side, computing occupied bounds
-    for x in [0...inventory.width]
-      isOccupied = false
-      for y in [0...inventory.height]
-        isOccupied = true if inventory.get(y + x * inventory.width)?
-      break if isOccupied
-    firstRow = x
+        if (!global.CraftingThesaurus_instance.matchesName(expectedName, actualPile)) {
+          return undefined;
+        }
 
-    for x in [inventory.width - 1..0]
-      isOccupied = false
-      for y in [0...inventory.height]
-        isOccupied = true if inventory.get(y + x * inventory.width)?
-      break if isOccupied
-    lastRow = x
+        // add original index from inputInventory for removing ingredients
+        const unshiftedIndex = (j + shiftColumn) + (i + shiftRow) * inputInventory.width;
+        foundIndices.push(unshiftedIndex);
+      }
+    }
 
+    return foundIndices;
+  }
 
-    for y in [0...inventory.height]
-      isOccupied = false
-      for x in [0...inventory.width]
-        isOccupied = true if inventory.get(y + x * inventory.width)?
-      break if isOccupied
-    firstColumn = y
+  // tighten the bounds around an inventory, shrinking it from the edges if possible
+  static tighten(inventory) {
+    // iterate from each side, computing occupied bounds
+    let x;
+    for (x = 0; x <= inventory.width; ++x) {
+      let isOccupied = false;
+      for (let y = 0; y <= inventory.height; ++y) {
+        if (inventory.get(y + x * inventory.width) !== undefined) {
+          isOccupied = true;
+        }
+      }
 
-    for y in [inventory.height - 1..0]
-      isOccupied = false
-      for x in [0...inventory.width]
-        isOccupied = true if inventory.get(y + x * inventory.width)?
-      break if isOccupied
-    lastColumn = y
+      if (isOccupied) break;
+    }
 
-    #console.log 'firstRow',firstRow,'lastRow',lastRow
-    #console.log 'firstColumn',firstColumn,'lastColumn',lastColumn
+    const firstRow = x;
 
-    # copy to new smaller-or-equal inventory
-    newWidth = lastColumn - firstColumn + 1
-    newHeight = lastRow - firstRow + 1
-
-    if newWidth < 0 or newHeight < 0
-      # completely empty inventory - but cannot shrink to 0x0 (dimensions must be
-      # positive in inventory >=0.1.2), so go with the next best thing (1x1)
-      newWidth = newHeight = 1
-
-    #console.log 'new ',newWidth,newHeight
-    newInventory = new inventory.constructor(newWidth, newHeight)
-    for oldX in [firstRow..lastRow]
-      for oldY in [firstColumn..lastColumn]
-        pile = inventory.get(oldY + oldX * inventory.width)
-        newX = oldX - firstRow
-        newY = oldY - firstColumn
-        newInventory.set newY + newX * newInventory.width, pile
-
-    return [newInventory, firstRow, firstColumn]
-
-  computeOutput: (inventory) ->
-    return @output.clone() if @findMatchingSlots(inventory) != undefined
-    undefined
-
-  # TODO: refactor with AmorphousRecipe
-  craft: (inventory) ->
-    slots = @findMatchingSlots(inventory)
-    return undefined if !slots
-
-    for slot in slots
-      inventory.takeAt slot, 1 # TODO: check return
-
-    return @output.clone()
+    for (x = inventory.width - 1; x > 0; --x) {
+      let isOccupied = false;
+      for (let y = 0; y <= inventory.height; ++y) {
+        if (inventory.get(y + x * inventory.width) !== undefined) {
+          isOccupied = true;
+        }
+      }
+      if (isOccupied) break;
+    }
+    const lastRow = x;
 
 
-class RecipeList
-  constructor: () ->
-    @recipes = []
+    let y;
+    for (y = 0; y <= inventory.height; ++y) {
+      let isOccupied = false;
+      for (let x = 0; x <= inventory.width; ++x) {
+        if (inventory.get(y + x * inventory.width) !== undefined) {
+          isOccupied = true;
+        }
+      }
+      if (isOccupied) break;
+    }
 
-  register: (recipe) ->
-    @recipes.push(recipe)
+    const firstColumn = y;
 
-  find: (inventory) ->
-    for recipe in @recipes
-      return recipe if recipe.computeOutput(inventory) != undefined
-    return undefined
+    for (let y = inventory.height - 1; y > 0; --y) {
+      let isOccupied = false;
+      for (let x = 0; x <= inventory.width; ++x) {
+        if (inventory.get(y + x * inventory.width) !== undefined) {
+          isOccupied = true;
+        }
+      }
+      if (isOccupied) break;
+    }
+    const lastColumn = y;
 
-  craft: (inventory) ->
-    for recipe in @recipes
-      output = recipe.craft(inventory)
-      return output if output
-    return undefined
+    //console.log('firstRow',firstRow,'lastRow',lastRow);
+    //console.log('firstColumn',firstColumn,'lastColumn',lastColumn);
 
-module.exports = (game, opts) ->
-  throw new Error 'craftingrecipes plugin (plugin only) replaced by voxel-recipes'
+    // copy to new smaller-or-equal inventory
+    let newWidth = lastColumn - firstColumn + 1;
+    let newHeight = lastRow - firstRow + 1;
 
-module.exports.Recipe = Recipe
-module.exports.AmorphousRecipe = AmorphousRecipe
-module.exports.PositionalRecipe = PositionalRecipe
-module.exports.CraftingThesaurus = CraftingThesaurus
-module.exports.RecipeList = RecipeList
+    if (newWidth < 0 || newHeight < 0) {
+      // completely empty inventory - but cannot shrink to 0x0 (dimensions must be
+      // positive in inventory >=0.1.2), so go with the next best thing (1x1)
+      newWidth = newHeight = 1;
+    }
+
+    //console.log('new ',newWidth,newHeight);
+    const newInventory = new inventory.constructor(newWidth, newHeight);
+    for (let oldX = firstRow; oldX < lastRow; ++oldX) {
+      for (let oldY = firstColumn; oldY < lastColumn; ++oldY) {
+        const pile = inventory.get(oldY + oldX * inventory.width);
+        const newX = oldX - firstRow;
+        const newY = oldY - firstColumn;
+        newInventory.set(newY + newX * newInventory.width, pile);
+      }
+    }
+
+    return [newInventory, firstRow, firstColumn];
+  }
+
+  computeOutput(inventory) {
+    if (this.findMatchingSlots(inventory) !== undefined) {
+      return this.output.clone();
+    }
+    return undefined;
+  }
+
+  // TODO: refactor with AmorphousRecipe
+  craft(inventory) {
+    const slots = this.findMatchingSlots(inventory);
+    if (!slots) return undefined;
+
+    for (let slot of slots) {
+      inventory.takeAt(slot, 1); // TODO: check return
+    }
+
+    return this.output.clone();
+  }
+}
+
+class RecipeList {
+  constructor() {
+    this.recipes = [];
+  }
+
+  register(recipe) {
+    this.recipes.push(recipe);
+  }
+
+  find(inventory) {
+    for (let recipe of this.recipes) {
+      if (recipe.computeOutput(inventory) !== undefined) {
+        return recipe;
+      }
+    }
+
+    return undefined;
+  }
+
+  craft(inventory) {
+    for (let recipe of this.recipes) {
+      const output = recipe.craft(inventory);
+      if (output) return output;
+    }
+    return undefined;
+  } 
+}
+
+module.exports = (game, opts) => {
+  throw new Error('craftingrecipes plugin (plugin only) replaced by voxel-recipes');
+};
+
+module.exports.Recipe = Recipe;
+module.exports.AmorphousRecipe = AmorphousRecipe;
+module.exports.PositionalRecipe = PositionalRecipe;
+module.exports.CraftingThesaurus = CraftingThesaurus;
+module.exports.RecipeList = RecipeList;
 
